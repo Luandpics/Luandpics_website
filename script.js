16f87883e46e86551ebb70bc5e08f86256e61bcb
lukasegouin <segouinluka@gmail.com>
2025-05-23 20:08:54 +0200

    adding style photos

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function showPage(page) {
 }
 
 const images = [
-    { src: 'Sport/perche.JPG', text: 'Sports' },
+    { src: 'Sport/perche2.JPG', text: 'Sports' },
     { src: 'Paysages/paysage.JPG', text: 'Paysages' },
     { src: 'Animaux/animaux.JPG', text: 'Animaux' },
     { src: 'Portraits/portrait.JPG', text: 'Portraits' },
